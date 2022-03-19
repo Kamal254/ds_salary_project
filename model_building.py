3c143f0e39cf8354bae0a7d5bcc2d196b35a0f0d
kamal <mrrobotpc123@gmail.com>
2022-03-19 11:04:29 +0530

    created flask api endpoint

--- model_building.py
+++ model_building.py
@@ -95,7 +95,6 @@ with open(file_name, 'rb') as pickled:
     data = pickle.load(pickled)
     model = data['model']
 
-model.predict(np.array(list(X_test.iloc[1,:])).reshape(1,-1))[0]
+print(model.predict(np.array(list(X_test.iloc[1,:])).reshape(1,-1))[0])
 
 list(X_test.iloc[1,:])
-
